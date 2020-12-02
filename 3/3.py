efa0d81525c2aa0fd75837ce215ebd0eeb653660
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-12-02 22:16:44 +0100

    Day3 initial commit

---
+++ 3/3.py
@@ -0,0 +1,11 @@
+def main():
+
+    processed_input = []
+    with open("3/Input.txt") as input_file:
+        for line in input_file.readlines():
+
+            processed_input.append(line)
+
+
+
+main()
