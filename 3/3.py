deef990dc5cb74b2eee787b495387d6ab83af981
JestemStefan <pudipudi2x@gmail.com>
2020-12-03 11:30:43 +0100

    Day3 Task2 Done

--- 3/3.py
+++ 3/3.py
@@ -8,11 +8,27 @@ def main():
             processed_input.append(line)
 
             #print(line)
-    
+
+    #Task1
     number_of_trees = slope_path(processed_input)
+
     print(number_of_trees)
 
 
+    answer = 1
+
+    for slope_dimensions in [[1,1], [1,3], [1,5], [1,7], [2,1]]:
+        
+        #                             v---Map           v--- Slope length   v--- Slope height (tilt)
+        number_of_trees = slope_path2(processed_input, slope_dimensions[1], slope_dimensions[0])
+
+        answer *= number_of_trees
+
+    print("Answer is: " + str(answer))
+        
+
+
+# Task 1
 def slope_path(input_map):
 
     char_index = 0
@@ -22,11 +38,32 @@ def slope_path(input_map):
 
         char_index = (3 * index) % 31 
 
-        if input_map[index][char_index] == "#":
+        if row[char_index] == "#":
             tree_counter += 1
 
     return(tree_counter)
 
+
+# Task2
+def slope_path2(input_map, slope_length, slope_height):
+
+
+    char_index = 0
+    tree_counter = 0
+
+    for row_index in range(0, len(input_map), slope_height):
+
+        current_row = input_map[row_index]
+        row_length = len(current_row) - 1
+
+        # check if character in this position is a tree ("#")
+        if current_row[char_index] == "#":
+            tree_counter += 1
+
+        # move down the slope
+        char_index = (char_index + slope_length) % row_length
+        
+    return(tree_counter)
 
 
 
