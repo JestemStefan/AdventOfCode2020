f16dc7aa56318d2cf5e31ebf3b2ce68db87b0ff1
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-12-06 17:05:13 +0100

    Day6 Task2 Done

--- 6/6.py
+++ 6/6.py
@@ -3,11 +3,41 @@ def main():
 
     with open("6/Input.txt") as input_file:
 
-            for line in input_file.readlines():
+        answers_groups = []
+        batch = []
 
-                line = line.strip()
-                print(line)
+        for line in input_file.readlines():
 
+            line = line.strip()
+
+            if line != "":
+                batch.append(set(line))
+
+
+            else:
+                answers_groups.append(batch)
+                #print(batch)
+                batch = []
+
+        else:
+            answers_groups.append(batch)
+            #print(batch)
+            batch = []
+
+
+        task2_answer = 0
+
+        for group in answers_groups:
+            task2_answer += len(set.intersection(*group))
+
+        print(task2_answer)
+
+    #task1_answer = 0
+
+    #for answers in answers_groups:
+        #task1_answer += len(set(answers))
+
+    #print(task1_answer)
 
 
 main()
