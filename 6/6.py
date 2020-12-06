594bfc0657a250006e8175aa0a9944936ea58e0f
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-12-06 16:15:40 +0100

    Day6 Task1 Initial Commit

---
+++ 6/6.py
@@ -0,0 +1,13 @@
+def main():
+
+
+    with open("6/Input.txt") as input_file:
+
+            for line in input_file.readlines():
+
+                line = line.strip()
+                print(line)
+
+
+
+main()
