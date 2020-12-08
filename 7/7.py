c12a120d6e103145da1aa259d06da4c742ce5ce7
JestemStefan <pudipudi2x@gmail.com>
2020-12-08 15:28:50 +0100

    Day7 Task1

--- 7/7.py
+++ 7/7.py
@@ -3,18 +3,30 @@ def main():
     processed_input = []
 
     with open("7/Input.txt") as input_file:
+        pattern_dict = {}
+        color_dict = {}
 
         for line in input_file.readlines():
 
-            line = line.strip()
-            #line = line.replace("/n", "")
-            #print("line: " + line)
+            line = line.strip().split()
+
+            # get pattern and save it to the dict
+            pattern = line[0]
+            color = line[1]
+            color_dict[color] = []
+
+            pattern_dict[pattern] = color_dict
+
             processed_input.append(line)
 
-    processed_input_sorted = sorted(processed_input)
 
     for line in processed_input:
         print(line)
+
+    for key in pattern_dict.keys():
+        pass
+        #print(key)
+        #print(pattern_dict[key])
 
 
 
