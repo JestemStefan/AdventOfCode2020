d852ee64313eebcb00cde98b87f6711cb2aadea9
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-12-10 23:21:04 +0100

    Day9 Task1 Done

---
+++ 9/9.py
@@ -0,0 +1,51 @@
+import cProfile as cp
+
+def main():
+
+    processed_input = []
+
+    with open("9/Input.txt") as input_file:
+        
+        for line in input_file.readlines():
+            line = line.strip()
+
+            processed_input.append(int(line))
+
+    loop = False
+    preamble = 25
+    number_index = 0
+
+    while loop != True:
+
+        loop = check_if_NOT_sum(processed_input, number_index, preamble)
+        number_index += 1
+
+
+
+        
+
+
+
+def check_if_NOT_sum(number_input, number_index, preamble):
+
+    test_number = number_input[number_index + preamble]
+    #print(test_number)
+
+    find_sum_here = number_input[number_index:preamble + number_index]
+
+    for num in find_sum_here:
+
+        reminder_num = abs(test_number - num)
+        #print(reminder_num)
+
+        if reminder_num in find_sum_here:
+            return False
+
+    else:
+        print("number index: " + str(number_index + preamble))
+        print("answer number: " + str(test_number))
+        return True
+    
+    
+cp.run("main()")
+#main()
