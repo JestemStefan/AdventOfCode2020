cf52b12243c606b58a184d6350b1a438df12dda6
JestemStefan <pudipudi2x@gmail.com>
2020-12-11 09:00:18 +0100

    Day9 Task2 Done

--- 9/9.py
+++ 9/9.py
@@ -23,7 +23,24 @@ def main():
         number_index += 1
 
     invalid_number = processed_input[number_index + preamble - 1] 
-    print(invalid_number)
+    max_search_index = number_index + preamble - 1
+
+    #print([max_search_index, invalid_number])
+
+
+    num_input_list = processed_input[:max_search_index]
+    num_input_list.reverse()
+
+    loop = False
+    number_index = 0
+    for idx, number in enumerate(num_input_list):
+
+        return_value = find_continous_sum(num_input_list, idx, invalid_number)
+
+        if return_value:
+            print("Answer: " + str(min(return_value) + max(return_value)))
+
+    
 
 
         
@@ -48,6 +65,28 @@ def check_if_NOT_sum(number_input, number_index, preamble):
     else:
         return True
     
+
+def find_continous_sum(num_input, start_index, target):
+    #print("target: " + str(target))
+
+    end_index = start_index + 1
+
+    continous_sum = 0
+
+    while continous_sum != target:
+
+        continous_sum = sum(num_input[start_index:end_index])
     
-#cp.run("main()")
-main()
+        if continous_sum > target:
+            return 0
+
+        elif continous_sum < target:
+            end_index += 1
+
+
+    return num_input[start_index:end_index]
+
+
+
+cp.run("main()")
+#main()
