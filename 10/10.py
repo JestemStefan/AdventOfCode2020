285c6b9ed54062d8fee4959c95ee189b0fe9ed5d
JestemStefan <pudipudi2x@gmail.com>
2020-12-11 16:10:27 +0100

    asa

--- 10/10.py
+++ 10/10.py
@@ -1,12 +1,43 @@
+import cProfile
+
+
 def main():
 
-    #processed_input = []
+    processed_input = []
 
     with open("10/Input.txt") as input_file:
         
         for line in input_file.readlines():
             line = line.strip()
-            print(line)
-            #processed_input.append(int(line))
+            
+            processed_input.append(int(line))
 
-main()
+    processed_input.append(max(processed_input) + 3)
+    processed_input = sorted(processed_input)
+
+    amount_of_one_jolts = 0
+    amount_of_three_jolts = 0
+
+    for i, adapter in enumerate(processed_input):
+        print(adapter)
+        difference = 0
+
+        if i == 0:
+            difference = adapter
+
+        else:
+            difference = (adapter - processed_input[i-1])
+
+        if difference < 3:
+            amount_of_one_jolts += 1
+            
+        else:
+            amount_of_three_jolts += 1
+
+    print("Answer: " + str(amount_of_one_jolts) + " * " + str(amount_of_three_jolts) + " = " + str(amount_of_one_jolts * amount_of_three_jolts))
+    #print(amount_of_one_jolts * amount_of_three_jolts)
+
+
+
+cProfile.run("main()")
+#main()
