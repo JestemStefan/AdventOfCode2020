6487245365149503a7a21a9f18f70eb8408f8575
JestemStefan <pudipudi2x@gmail.com>
2020-12-14 15:01:14 +0100

    Day10 Task2 Working

--- 10/10.py
+++ 10/10.py
@@ -19,7 +19,7 @@ def main():
     amount_of_three_jolts = 0
 
     for i, adapter in enumerate(processed_input):
-        print(adapter)
+        #print(adapter)
         difference = 0
 
         if i == 0:
@@ -37,7 +37,26 @@ def main():
     print("Answer: " + str(amount_of_one_jolts) + " * " + str(amount_of_three_jolts) + " = " + str(amount_of_one_jolts * amount_of_three_jolts))
     #print(amount_of_one_jolts * amount_of_three_jolts)
 
+    solution = {0:1}
+
+    for line in processed_input:
+        
+        solution[line] = 0
+
+        if line - 1 in solution:
+            solution[line] += solution[line-1]
+            #print(solution[line])
+
+        if line - 2 in solution:
+            solution[line] += solution[line-2]
+            #print(solution[line])
+
+        if line - 3 in solution:
+            solution[line] += solution[line-3]
+            #print(solution[line])
+
+    print(solution[max(processed_input)])
 
 
-cProfile.run("main()")
-#main()
+#cProfile.run("main()")
+main()
