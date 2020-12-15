776ff4a382656b9f1173999838ea660e4abc7337
JestemStefan <pudipudi2x@gmail.com>
2020-12-15 10:56:22 +0100

    Day11 Task1 Initial commit

---
+++ 11/11.py
@@ -0,0 +1,17 @@
+
+
+def main():
+
+    #processed_input = []
+
+    with open("11/Input.txt") as input_file:
+        
+        for line in input_file.readlines():
+            line = line.strip()
+            print(line)
+
+
+
+
+
+main()
