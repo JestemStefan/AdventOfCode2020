5e61a4b688ce96091118e2b2c1c9a096f984bf72
JestemStefan <pudipudi2x@gmail.com>
2020-12-15 15:56:52 +0100

    Day11 Task1 Early setup

--- 11/11.py
+++ 11/11.py
@@ -1,17 +1,38 @@
-
+import cProfile
 
 def main():
 
-    #processed_input = []
+    processed_input = []
 
     with open("11/Input.txt") as input_file:
         
         for line in input_file.readlines():
             line = line.strip()
-            print(line)
+
+            processed_input.append(line)
 
 
+    #for line in processed_input:
+        #print(line)
 
 
+    new_setup = []
+
+    for line in processed_input:
+
+        new_line = ""
+
+        for char in line:
+            if char == "L":
+                char = "#"
+
+            new_line += char
+
+        new_setup.append(new_line)
+
+
+    for line in new_setup:
+        print(line)
 
 main()
+#cProfile.run("main()")
